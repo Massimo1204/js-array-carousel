e8f0e7f91ec306897d6180ebe9f30c270368fb49
Massimo1204 <xiangmassimo@gmail.com>
2022-03-01 20:58:44 +0100

    descriptions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const images = ['<img src="/img/01.jpg"  alt="img calm lake">' ,'<img src="/img/02.jpg"  alt="img Svizzera">', '<img src="/img/03.jpg"  alt="img London"></img>','<img src="/img/04.jpg"  alt="img city"></img>','<img src="/img/05.jpg"  alt="img beach">'];
+const descriptions = ["<h3>Lago</h3><p>Solo, di fronte ad un immenso lago fui ammirando e riflettendo..</p>","<h3>Svizzera</h3><p>Lorem ipsum</p>","<h3>Londra</h3><p>Come and visit 221b of Baker Street</p>","<h3>City bello</h3><p>Somewhere in the world</p>","<h3>Beach</h3><p>Weeeeeeeeeeeeeee</p>"];
 const mainImageWrapper = document.getElementById('main-img-wrapper');
 const sideImageWrapper = document.getElementById('side-img-wrapper');
 let sideImageSelector;
@@ -7,6 +8,8 @@ const buttonUp = document.getElementById('my-button-up');
 let counter = 0;
 let sideImageCounter = 1 ; 
 mainImageWrapper.innerHTML = images[0];
+mainImageWrapper.innerHTML += descriptions[0];
+
 
 for(let i=0; i<5 ; i++){
     sideImageWrapper.innerHTML += images[i];
@@ -26,6 +29,7 @@ function changeImageDown(){
     }
     sideImageSelectorPrevious.classList.remove('active');
     mainImageWrapper.innerHTML = images[counter];
+    mainImageWrapper.innerHTML += descriptions[counter];
     sideImageSelector = sideImageWrapper.querySelector('img:nth-child('+sideImageCounter+')');
     sideImageSelectorPrevious = sideImageSelector;
     sideImageSelector.classList.add('active');
@@ -42,6 +46,7 @@ function changeImageUp(){
     }
     sideImageSelectorPrevious.classList.remove('active');
     mainImageWrapper.innerHTML = images[counter];
+    mainImageWrapper.innerHTML += descriptions[counter];
     sideImageSelector = sideImageWrapper.querySelector('img:nth-child('+sideImageCounter+')');
     sideImageSelectorPrevious = sideImageSelector;
     sideImageSelector.classList.add('active');
