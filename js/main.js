2ba40f1ffec3befb2abb8e7decbb3cf2520f8c40
Massimo1204 <xiangmassimo@gmail.com>
2022-03-01 16:38:02 +0100

    somewhere near something

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 const images = ['<img src="/img/01.jpg"  alt="img calm lake">' ,'<img src="/img/02.jpg"  alt="img Svizzera">', '<img src="/img/03.jpg"  alt="img London"></img>','<img src="/img/04.jpg"  alt="img city"></img>','<img src="/img/05.jpg"  alt="img beach">'];
 const mainImageWrapper = document.getElementById('main-img-wrapper');
+const sideImageWrapper = document.getElementById('side-img-wrapper');
+let sideImageSelector;
 const buttonDown = document.getElementById('my-button-down');
 const buttonUp = document.getElementById('my-button-up');
 let counter = 1;
 
 mainImageWrapper.innerHTML = images[0];
 
+for(let i=0; i<5 ; i++){
+    sideImageWrapper.innerHTML += images[i];
+}
+
 function changeImageDown(){
     mainImageWrapper.innerHTML = images[counter];
+    sideImageSelector = sideImageWrapper.querySelector('img:nth-child('+counter+++')');
+    console.log(sideImageSelector);
     if(counter >= 4 ){
         counter = 0;
     }
     counter++;
-
 }
 
 function changeImageUp(){
